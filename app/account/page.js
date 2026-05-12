d041a675d64313905c2623db00b3ca50222d20cb
hazeen dodhiya <hazeenwork@gmail.com>
2026-05-12 17:44:13 +0500

    user bearer

--- app/account/page.js
+++ app/account/page.js
@@ -281,7 +281,7 @@ export default function AccountPage() {
         {
           method: "DELETE",
           headers: {
-            Authorization: token,
+            Authorization: `Bearer ${token}`
           },
         }
       );
